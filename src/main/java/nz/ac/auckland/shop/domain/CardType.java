30aade56658397bb7d4c4fc3dc4124bd3915845d
sabflik <szaf045@aucklanduni.ac.nz>
2016-09-07 00:35:38 +1200

    CardType class fully implemented
    fromString method added to CardType

--- src/main/java/nz/ac/auckland/shop/domain/CardType.java
+++ src/main/java/nz/ac/auckland/shop/domain/CardType.java
@@ -2,4 +2,15 @@ package nz.ac.auckland.shop.domain;
 
 public enum CardType {
 	CREDIT, DEBIT;
+	
+	public static CardType fromString(String text) {
+	    if (text != null) {
+	      for (CardType ct : CardType.values()) {
+	        if (text.equalsIgnoreCase(ct.toString())) {
+	          return ct;
+	        }
+	      }
+	    }
+	    return null;
+	  }
 }
